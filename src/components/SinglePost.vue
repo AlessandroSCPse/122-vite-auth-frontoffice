a4bcc65b7cbfa2775468bb2b544f428d26a043fc
AlessandroSCPse <scolozzi.alessandro@gmail.com>
2024-06-10 11:53:15 +0200

    added category and tags to singlePost card

--- src/components/SinglePost.vue
+++ src/components/SinglePost.vue
@@ -13,6 +13,13 @@ export default {
             <!-- <img src="..." class="card-img-top" alt="..."> -->
             <div class="card-body">
                 <h5 class="card-title">{{ postDetails.title }}</h5>
+                <div v-if="postDetails.category">
+                    <strong>Categoria</strong>: {{ postDetails.category.name }}
+                </div>
+
+                <div v-if="postDetails.tags.length > 0">
+                    <strong>Tags</strong>: <span v-for="tag in postDetails.tags">{{ tag.name }} &nbsp;</span>
+                </div>
                 <p v-if="postDetails.content" class="card-text">{{ postDetails.content }}</p>
                  <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
             </div>
