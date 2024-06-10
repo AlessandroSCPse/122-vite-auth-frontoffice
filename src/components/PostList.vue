f7396e835e75ff6047406022fa1245af8458e9f7
AlessandroSCPse <scolozzi.alessandro@gmail.com>
2024-06-10 12:51:36 +0200

    pagination

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -9,19 +9,29 @@ export default {
     },
     data() {
         return {
+            currentPage: 1,
+            prevPageUrl: null,
+            nextPageUrl: null,
             posts: []
         };
     },
     methods: {
-        getPosts() {
-            axios.get('http://127.0.0.1:8000/api/posts')
+        getPosts(pageNumber) {
+            axios.get('http://127.0.0.1:8000/api/posts', {
+                params: {
+                    page: pageNumber
+                }
+            })
             .then((response) => {
-                this.posts = response.data.results;
+                this.posts = response.data.results.data;
+                this.currentPage = response.data.results.current_page;
+                this.prevPageUrl = response.data.results.prev_page_url;
+                this.nextPageUrl = response.data.results.next_page_url;
             });
         }
     },
     mounted() {
-        this.getPosts();
+        this.getPosts(this.currentPage);
     }
 }
 </script>
@@ -29,10 +39,21 @@ export default {
 <template>
     <div class="container">
         <h1>Tutti i post</h1>
-
+        {{ currentPage }}
         <div class="row row-cols-3">
             <SinglePost v-for="post in posts" :postDetails="post" :key="post.id"></SinglePost>
         </div>
+
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li v-if="prevPageUrl" class="page-item">
+                    <a class="page-link" @click="getPosts(currentPage - 1)">Previous</a>
+                </li>
+                <li v-if="nextPageUrl" class="page-item">
+                    <a class="page-link" @click="getPosts(currentPage + 1)">Next</a>
+                </li>
+            </ul>
+        </nav>
     </div>
 </template>
 
