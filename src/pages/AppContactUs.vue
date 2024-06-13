fd88dbb06dec023579cca3e84435928947d92aef
AlessandroSCPse <scolozzi.alessandro@gmail.com>
2024-06-13 10:35:30 +0200

    done contact form layout

--- src/pages/AppContactUs.vue
+++ src/pages/AppContactUs.vue
@@ -1,11 +1,18 @@
 <script>
+import AppContactForm from '../components/AppContactForm.vue';
+
 export default {
-    name: 'AppContactUs'
+    name: 'AppContactUs',
+    components: {
+        AppContactForm
+    }
 }
 </script>
 
 <template>
     <div class="container">
         <h1>Contact us</h1>
+
+        <AppContactForm></AppContactForm>
     </div>
 </template>
